75b3e9fa8324b7bca86029eb3d1ff12cdce12480
Sheldon Hull <sheldonhull@users.noreply.github.com>
2022-10-03 13:41:29 -0500

    style: format

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -3,12 +3,16 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/DelineaXPM/dsv-gitlab/magefiles/constants"
 
 	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 	"github.com/pterm/pterm"
 	"github.com/sheldonhull/magetools/ci"
+	"github.com/sheldonhull/magetools/pkg/req"
 	"github.com/sheldonhull/magetools/tooling"
 
 	// mage:import
@@ -19,7 +23,12 @@ import (
 	"github.com/sheldonhull/magetools/precommit"
 	//mage:import
 	_ "github.com/sheldonhull/magetools/secrets"
+
+	"github.com/bitfield/script"
 )
+
+// Test contains mage tasks for testing.
+type Test mg.Namespace
 
 // createDirectories creates the local working directories for build artifacts and tooling.
 func createDirectories() error {
@@ -36,16 +45,20 @@ func createDirectories() error {
 }
 
 // Init runs multiple tasks to initialize all the requirements for running a project for a new contributor.
-func Init() error { //nolint:deadcode // Not dead, it's alive.
+func Init() error {
 	pterm.DefaultHeader.Println("running Init()")
-
 	mg.SerialDeps(
 		Clean,
 		createDirectories,
-		(gotools.Go{}.Tidy),
-		(gotools.Go{}.Init),
 	)
 
+	mg.Deps(
+		(gotools.Go{}.Tidy),
+		// (InstallSyft),.
+	)
+	if err := tooling.SilentInstallTools(CIToolList); err != nil {
+		return err
+	}
 	if ci.IsCI() {
 		pterm.Debug.Println("CI detected, done with init")
 		return nil
@@ -57,9 +70,29 @@ func Init() error { //nolint:deadcode // Not dead, it's alive.
 	}
 	// These can run in parallel as different toolchains.
 	mg.Deps(
+		(gotools.Go{}.Init),
 		(gittools.Gittools{}.Init),
 		(precommit.Precommit{}.Init),
+		(InstallTrunk),
 	)
+	return nil
+}
+
+// InstallTrunk installs trunk.io tooling.
+func InstallTrunk() error {
+	_, err := script.Exec("curl https://get.trunk.io -fsSL").Exec("bash").Stdout()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// InstallSyft installs SBOM tooling for goreleaser.
+func InstallSyft() error {
+	_, err := script.Exec("curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh").Exec("sh -s -- -b /usr/local/bin").Stdout()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -74,4 +107,72 @@ func Clean() {
 		pterm.Success.Printf("🧹 [%s] dir removed\n", dir)
 	}
 	mg.Deps(createDirectories)
+}
+
+// ActIntegration runs local act cli to test the integration.
+func (Test) ActIntegration() error {
+	binary, err := exec.LookPath("act")
+	if err != nil {
+		pterm.Error.Println("unable to resolve act as installed. please install via directions here: https://github.com/nektos/act#installation")
+		return err
+	}
+
+	return sh.RunV(
+		binary,
+		"--job", "integration",
+		"--secret-file", constants.SecretFile,
+	)
+}
+
+// Unit runs go unit tests.
+func (Test) Unit() {
+	mg.Deps(gotools.Go{}.TestSum("./..."))
+}
+
+// PullImage pulls the latest docker image published to the hub for testing.
+func PullImage() error {
+	if err := sh.RunV("docker", "pull", constants.DockerRegistryPathQualified+":"+constants.DockerTag); err != nil {
+		pterm.Error.Printfln("unable to pull docker image: %v", err)
+		return err
+	}
+	return nil
+}
+
+// TestIntegration runs local act cli to test the integration.
+func (Test) Integration() error {
+	// Mg.Deps(PullImage).
+	currentDescribedTag, err := sh.Output("git", "describe", "--tags", "--abbrev=0")
+	if err != nil {
+		pterm.Error.Printfln("unable to retrieve tag: %v", err)
+		return err
+	}
+	pterm.Info.Printfln("using tag: %s", currentDescribedTag)
+	pterm.Info.Println("running docker now")
+
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	return sh.RunV(
+		"docker",
+		"run",
+		"--env-file", constants.SecretFile,
+		// NOTE: Optional way to invoke: 	"--env", "DSV_RETRIEVE="+"{"secretPath": "ci:tests:dsv-gitlab", "secretKey": "secret-01", "outputVariable": "RETURN_VALUE_1"}",.
+		"--rm",
+		"-v", filepath.Join(wd, ".cache")+":"+"/app/.cache:rw",
+		constants.LocalDockerRegistryPathQualified+":"+currentDescribedTag,
+	)
+}
+
+func Changelog() error {
+	binary, err := req.ResolveBinaryByInstall("git-chglog", "github.com/git-chglog/git-chglog/cmd/git-chglog@latest")
+	if err != nil {
+		pterm.Error.Println("unable to install binary")
+		return err
+	}
+
+	return sh.RunV(
+		binary,
+		"--output", "CHANGELOG.md",
+	)
 }
