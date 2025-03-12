1aa0b45ac12472bea4cd3af48483e4f09c76780d
Bill Hamilton <bill.hamilton@delinea.com>
2025-03-12 15:57:38 +0000

    udpated install-gitlab-ci.sh script

--- .devcontainer/library-scripts/install-gitlab-ci.sh
+++ .devcontainer/library-scripts/install-gitlab-ci.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -s "https://firecow.github.io/gitlab-ci-local/ppa/pubkey.gpg" | sudo apt-key add -
-sudo curl -s -o /etc/apt/sources.list.d/gitlab-ci-local.list "https://firecow.github.io/gitlab-ci-local/ppa/gitlab-ci-local.list"
+curl -s "https://gitlab-ci-local-ppa.firecow.dk/pubkey.gpg" | sudo apt-key add -
+
+echo "deb https://gitlab-ci-local-ppa.firecow.dk ./" | sudo tee /etc/apt/sources.list.d/gitlab-ci-local.list
+
 sudo apt-get -yqq update
 sudo apt-get -yqq install gitlab-ci-local
